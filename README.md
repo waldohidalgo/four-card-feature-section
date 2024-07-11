1d90ae68c53641b53e9400af01e91125d75e41f0
waldohidalgo <whidalgohp@gmail.com>
2024-07-10 20:40:34 -0400

    upload full project

---
+++ README.md
@@ -0,0 +1,35 @@
+# Four card feature section
+
+Repositorio con el código solución del challenge: [Four card feature section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK)
+
+## The challenge
+
+El desafío consiste en realizar lo siguiente:
+
+> Users should be able to:
+>
+> - View the optimal layout for the site depending on their device's screen size
+
+## Diseños a Replicar
+
+### 1-Mobile Design
+
+![Mobile Design](./screenshots/mobile-design.jpg)
+
+### 2-Desktop Design
+
+![Desktop Design](./screenshots/desktop-design.jpg)
+
+## Proyecto Realizado
+
+### 1-Mobile Size
+
+![Mobile Size](./screenshots/waldo/mobile.webp)
+
+### 2-Desktop Size
+
+![Desktop Size](./screenshots/waldo/desktop.webp)
+
+### 3-Tablet Size
+
+![Tablet Size](./screenshots/waldo/tablet.webp)
---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<script>
+import iconCalculator from "./assets/icon-calculator.svg";
+import iconSupervisor from "./assets/icon-supervisor.svg";
+import iconTeamBuilder from "./assets/icon-team-builder.svg";
+import iconKarma from "./assets/icon-karma.svg";
+import Footer from "./components/Footer.vue";
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          title: "Supervisor",
+          description: "Monitors activity to identify project roadblocks",
+          icon: iconSupervisor,
+          classColor: "border-cyan",
+        },
+        {
+          title: "Team Builder",
+          description:
+            "Scans our talent network to create the optimal team for your project",
+          icon: iconTeamBuilder,
+          classColor: "border-red",
+        },
+        {
+          title: "Karma",
+          description: "Regularly evaluates our talent to ensure quality",
+          icon: iconKarma,
+          classColor: "border-orange",
+        },
+        {
+          title: "Calculator",
+          description:
+            "Uses data from past projects to provide better delivery estimates",
+          icon: iconCalculator,
+          classColor: "border-blue",
+        },
+      ],
+    };
+  },
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <main
+    class="font-poppins py-[84px] px-[27px] bg-veryLightGray lg:ps-[11.5%] lg:pe-[10.5%] lg:py-[78px]"
+  >
+    <h2
+      class="text-veryDarkBlue text-center font-extralight text-[24.1px] tracking-[0.1px] lg:text-[36px]"
+    >
+      Reliable, efficient delivery
+    </h2>
+    <h1
+      class="text-veryDarkBlue font-semibold text-center text-[24px] lg:text-[36px]"
+    >
+      Powered by Technology
+    </h1>
+    <p
+      class="text-grayishBlue text-center mt-[16px] text-[15px] leading-6 lg:mt-[11px] lg:text-[15px] lg:w-[47%] lg:mx-auto"
+    >
+      Our Artificial Intelligence powered tools use millions of project data
+      points to ensure that your project is successful
+    </p>
+    <div
+      class="mt-[77px] flex flex-col gap-[30px] sm:flex-row sm:flex-wrap sm:justify-center lg:grid lg:grid-cols-3 lg:grid-rows-4 lg:mt-[64px]"
+    >
+      <div
+        v-for="(card, index) in cards"
+        class="p-[21px_26px_24px_29px] flex flex-col bg-white h-[216px] rounded-[5px] border-t-4 shadow-[0px_8px_9px_1px_rgba(0,0,0,0.1)] lg:p-[28px_31px_33px_33px] lg:h-[249px] sm:w-[35%] lg:w-full"
+        :class="card.classColor + ' ' + 'item-' + index"
+      >
+        <h4 class="text-[20px] font-semibold">{{ card.title }}</h4>
+        <p
+          class="mt-[5px] text-[13.3px] text-grayishBlue leading-[21px] lg:mt-[14px]"
+        >
+          {{ card.description }}
+        </p>
+        <div class="ms-auto mt-auto">
+          <img
+            class="w-[55px] lg:w-[63px]"
+            :src="card.icon"
+            :alt="card.title + ' icon'"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
+  <Footer />
+</template>
